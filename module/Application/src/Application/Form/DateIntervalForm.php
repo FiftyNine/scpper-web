8e1bd868d33b39d4607ed3c54fcdef34886573a8
FiftyNine <lixbart@mail.ru>
2017-03-01 19:43:40 +0700

    Responsive UI: "Changes" page rework. Everything is under tabs.

--- module/Application/src/Application/Form/DateIntervalForm.php
+++ module/Application/src/Application/Form/DateIntervalForm.php
@@ -19,35 +19,38 @@ class DateIntervalForm extends Form
             'type' => 'Zend\Form\Element\Text',
             'name' => self::FROM_DATE_NAME,
             'options' => array(
-                    'label' => 'From',
-//                    'format' => 'Y-m-d'
+                'label' => 'From',
+//              'format' => 'Y-m-d'
             ),
             'attributes' => array(
-                    'class' => 'form-control datepicker',
-//                    'min' => '2000-01-01',
-//                    'max' => '2020-01-01',
-//                    'step' => '1', // days; default step interval is 1 day
+                'class' => 'form-control datepicker',
+                'id' => 'date-interval-from'
+//              'min' => '2000-01-01',
+//              'max' => '2020-01-01',
+//              'step' => '1', // days; default step interval is 1 day
             )
         ));
         $this->add(array(
             'type' => 'Zend\Form\Element\Text',
             'name' => self::TO_DATE_NAME,
             'options' => array(
-                    'label' => 'To',
-//                    'format' => 'Y-m-d'
+                'label' => 'To',
+//              'format' => 'Y-m-d'
             ),
             'attributes' => array(
-                    'class' => 'form-control datepicker',
-  //                  'min' => '2000-01-01',
-//                    'max' => '2020-01-01',
-//                    'step' => '1', // days; default step interval is 1 day
+                'class' => 'form-control datepicker',
+                'id' => 'date-interval-to'
+  //            'min' => '2000-01-01',
+//              'max' => '2020-01-01',
+//              'step' => '1', // days; default step interval is 1 day
             )
         ));
         $this->add(array(
             'type' => 'submit',
             'name' => self::SUBMIT_NAME,
             'attributes' => array(
-                    'class' => 'btn btn-default',
+                'class' => 'btn btn-default',
+                'id' => 'date-interval-button'
             ),
             'value' => 'Go',
         ));        
